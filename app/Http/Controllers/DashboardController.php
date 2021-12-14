0ab8de8cdaceeabb5ecefb5587e2b257cb761122
Alex Caepio <mjbcaepio@gmail.com>
2021-12-14 07:14:44 +0300

    first

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function index(){
+        if(Auth::user()->hasRole('user')){
+            return view('userdash');
+        }elseif(Auth::user()->hasRole('blogwriter')){
+            return view('blogwriterdash');
+        }elseif(Auth::user()->hasRole('admin')){
+            return view('dashboard');
+        }
+    }
+}
